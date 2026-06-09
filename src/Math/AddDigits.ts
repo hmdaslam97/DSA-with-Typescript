c93e38820dd2472bcd183f7720b59557a8e2a742
Mohammed Aslam <mohammed.aslam@contractor.tranetechnologies.com>
2026-06-09 13:02:32 +0530

    Solved Add Digits problem

---
+++ src/Math/AddDigits.ts
@@ -0,0 +1,39 @@
+/*
+258. Add Digits
+Easy
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+
+Example 2:
+Input: num = 0
+Output: 0
+
+Constraints:
+0 <= num <= 231 - 1
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+Runtime 0 ms Beats 100.00% Memory 58.27 MB Beats 26.32%
+*/
+
+function addDigits(num: number): number {
+/*
+Approach
+Upon observing carefully, we can see that 38 -> 11 -> 2 is similar to 38 % 9 which is again 2
+
+Similarly, for any number which is a multiple of 9, the result will be 9. For example, 18 -> 9, 27 -> 9, 36 -> 9 and so on.
+
+Edge Case Scenario:
+If num == 0, then 0 % 9 will give 9 which is wrong as the correct answer should be 0. So, we need to handle it separately.
+ */
+  return num <= 0 ? num : num%9 ===0 ? 9 : num % 9;
+};
+
+console.log(addDigits(38)) // 2
+console.log(addDigits(0)) // 0
