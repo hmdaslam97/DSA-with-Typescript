3da2c57698890d09ac9d3956fa1f3cf65c6d6035
Mohammed Aslam <mohammed.aslam@contractor.tranetechnologies.com>
2026-06-10 12:57:03 +0530

    Solved a greedy algo problem

---
+++ src/Greedy/ArrayPartition.ts
@@ -0,0 +1,40 @@
+/*
+561. Array Partition
+Easy
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+Example 1:
+Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+
+Example 2:
+Input: nums = [6,2,6,5,1,2]
+Output: 9
+Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+Constraints:
+1 <= n <= 104
+nums.length == 2 * n
+-104 <= nums[i] <= 104
+
+Runtime 41 ms Beats 43.36%
+Memory 61.30 MB Beats 55.75%
+ */
+
+function arrayPairSum(nums: number[]): number {
+  nums.sort((a,b)=>a-b)
+  let minSum = 0;
+  for(let i=0; i<nums.length-1; i+=2){
+    minSum += nums[i]
+  }
+  return minSum;
+};
+
+console.log(arrayPairSum([1,5,2,8]))//3
+//console.log(arrayPairSum([1,4,3,2]))//4
+//console.log(arrayPairSum([6,2,6,5,1,2]))//9
